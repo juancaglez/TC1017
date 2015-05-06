ca3086ec5507f54d3b8283d55b57a89ad0994ce8
juancaglez <A01630498@itesm.mx>
2015-05-06 14:05:15 -0500

    wsq11

---
+++ wsq11.cpp
@@ -0,0 +1,37 @@
+// Juan Carlos González Hdez wsq11
+
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+void palin ()
+{
+	string x;
+	string y;
+	
+	cout<< "This program will tell you if the text is a palindrome or not " <<endl;
+	cout<< "Enter a text " <<endl;
+	cin>> x;
+	
+	y= string (x.rbegin(), x.rend());
+	cout<< "The reverse of the text " << y << endl;
+	
+	if (x==y)
+		{
+			cout<< "The text you enter is a palindrom " <<endl;
+		}
+	else
+		{
+			cout<< "The text you enter is not a palindrome " <<endl;
+		}
+		
+}
+
+int main ()
+{
+	palin ();
+	
+	return 0;
+}
